026a823386fdce8a1b0f1150aed9a9fe03879404
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2025-07-22 23:26:26 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,40 @@
+/*  firebase.js – Firestore 연결 및 helper */
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
 import {
-  getFirestore, collection, addDoc, query, orderBy, onSnapshot
+  getFirestore, collection, addDoc, serverTimestamp,
+  query, orderBy, onSnapshot
 } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 
 let db;
-export function initFirebase(){
+
+/* 0) 초기화 */
+export function initFirebase () {
   const firebaseConfig = {
-    apiKey: 'AIzaSyCfJ2xA0htQIlwvnFp5oTcKmEjXJLcj9Cs', authDomain: 'wedding-invitation-46ad9.web.app', projectId: 'wedding-invitation-46ad9'
+    apiKey            : 'AIzaSyCfJ2xA0htQIlwvnFp5oTcKmEjXJLcj9Cs',
+    authDomain        : 'wedding-invitation-46ad9.firebaseapp.com',
+    projectId         : 'wedding-invitation-46ad9',
+    appId             : '1:281364628692:web:c169a60fca936dca38a741'
   };
   db = getFirestore(initializeApp(firebaseConfig));
 }
 
-export const addGuestbook = (name,msg)=> addDoc(collection(db,'guestbook'),{name,msg,ts:Date.now()});
-export const addRsvp = data => addDoc(collection(db,'rsvp'),{...data,ts:Date.now()});
-export const listenGuestbook = cb => {
-  const q = query(collection(db,'guestbook'), orderBy('ts','desc'));
-  return onSnapshot(q, snap => cb(snap.docs.map(d=>d.data())));
+/* 1) 방명록 추가 */
+export const addGuestbook = (name, msg) =>
+  addDoc(collection(db, 'guestbook'), {
+    name, msg,
+    ts: serverTimestamp()
+  });
+
+/* 2) RSVP 추가 */
+export const addRsvp = data =>
+  addDoc(collection(db, 'rsvp'), {
+    ...data,
+    ts: serverTimestamp()
+  });
+
+/* 3) 방명록 실시간 감시 */
+export const listenGuestbook = (cb) => {
+  const q = query(collection(db, 'guestbook'), orderBy('ts', 'desc'));
+  return onSnapshot(q, snap => cb(snap.docs.map(d => d.data())));
 };
